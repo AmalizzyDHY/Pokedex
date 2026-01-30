3fe9b46438f8937acc622f5770647368db729498
Sam04-b <samuel.akintola@isen-ouest.yncrea.fr>
2026-01-30 01:41:58 +0100

    analyse

--- poulpe.py
+++ poulpe.py
@@ -1,11 +1,1 @@
-import pandas as pd
-
-# 1. Charger la base
-df = pd.read_csv('C:\Users\hp\OneDrive\Documents\Projet_python_avancé_M1\Pokedex\pokemon_data.csv')
-
-# 2. Vérifier les types et les manquants
-print("--- Vérification des colonnes ---")
-print(df.info()) # Montre si les colonnes sont bien des nombres ou du texte
-
-print("\n--- Vérification des Type 2 manquants ---")
-print(df['Type 2'].isnull().sum()) # Compte combien n'ont pas de 2ème type
+bonjour Lisa
