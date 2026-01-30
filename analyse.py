56613245a921dc78ba5ecbb8de7c070c2ac50547
Sam04-b <samuel.akintola@isen-ouest.yncrea.fr>
2026-01-30 02:00:40 +0100

    analyse_données

---
+++ analyse.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# 1. Charger la base
+df = pd.read_csv('data/pokemon_data.csv')
+
+# 2. Vérifier les types et les manquants
+print("--- Vérification des colonnes ---")
+print(df.info()) # Montre si les colonnes sont bien des nombres ou du texte
+
+print("\n--- Vérification des Type 2 manquants ---")
+print(df['Type 2'].isnull().sum()) # Compte combien n'ont pas de 2ème type
